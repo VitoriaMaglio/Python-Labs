201a5db84c09b4fe7e70c17872ebca55096c7aad
Vitória Valentina Maglio <vitoriamaglio@gmail.com>
2025-03-17 09:38:23 -0300

    Add files via upload

---
+++ aula.py
@@ -0,0 +1,7 @@
+
+#identação : e da enter; o curso vai para a outra linha
+
+#input : é o comando que recebe dados do usuário
+
+nome= input("Digite seu nome: ")
+print("Olá", nome, "!")
---
+++ python02.py
@@ -0,0 +1,5 @@
+#Exercícios
+ 
+ A = 11 
+ B = 3 
+ print(A+B*A) 
